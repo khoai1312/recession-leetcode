c967f66b8cb8984d8a7e13b9c2fdb8b28873a853
hanguyen <khoai1312@gmail.com>
2022-11-13 01:44:07 +0800

    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.

---
+++ src/main/ValidAnagram.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public static void main (String[] args) {
+        System.out.println(isValidAnagram("nagaram", "anagram"));
+        System.out.println(isValidAnagram("rat", "car"));
+    }
+
+    public static boolean isValidAnagram (String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> countCharacters = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (countCharacters.containsKey(currentChar)) {
+                countCharacters.put(currentChar, countCharacters.get(currentChar) + 1);
+            } else {
+                countCharacters.put(currentChar, 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char currentChar = t.charAt(i);
+            if (countCharacters.containsKey(currentChar)) {
+                countCharacters.put(currentChar, countCharacters.get(currentChar) - 1);
+            } else {
+                return false;
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : countCharacters.entrySet()) {
+            int count = entry.getValue();
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
